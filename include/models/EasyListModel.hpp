5d75adc89852613b0c0c7ef53cd698d8964fcdee
Guillaume M. <58004135+GuillaumeMZ@users.noreply.github.com>
2024-06-08 02:19:40 +0200

    feat: first working version of the EasyListModel utility class (name subject to change)

---
+++ include/models/EasyListModel.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <QAbstractListModel>
+#include <QList>
+
+template<typename T>
+class EasyListModel: public QAbstractListModel
+{
+    public:
+        void append(const T& value);
+        void clear();
+
+        int rowCount(const QModelIndex& parent = QModelIndex()) const override;
+        QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+        QHash<int, QByteArray> roleNames() const override;
+
+    private:
+        QList<T> items;
+};
+
+template<typename T>
+void EasyListModel<T>::append(const T& value)
+{
+    beginInsertRows(QModelIndex(), rowCount(), rowCount());
+    items.append(value);
+    endInsertRows();
+}
+
+template<typename T>
+void EasyListModel<T>::clear()
+{
+    beginRemoveRows(QModelIndex(), 0, rowCount() - 1);
+    items.clear();
+    endRemoveRows();
+}
+
+template<typename T>
+int EasyListModel<T>::rowCount(const QModelIndex& parent) const
+{
+    return items.count();
+}
+
+template<typename T>
+QVariant EasyListModel<T>::data(const QModelIndex& index, int role) const
+{
+    int actualIndex = index.row();
+    if(!index.isValid() || actualIndex < 0 || actualIndex >= items.count())
+    {
+        return {};
+    }
+
+    return QVariant::fromValue(items.at(actualIndex));
+}
+
+template<typename T>
+QHash<int, QByteArray> EasyListModel<T>::roleNames() const
+{
+    QHash<int, QByteArray> result;
+    result[Qt::UserRole + 1] = "item";
+
+    return result;
+}
